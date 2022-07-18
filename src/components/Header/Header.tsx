a8ce1f575a557eff6ad979311d2d4e4c925de2a1
Jelon <jelon1211@gmail.com>
2022-07-18 21:42:43 +0200

    Pokemon Render

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,18 +6,20 @@ const Header = () => {
     <nav className="nav-bar">
       <div className="nav-bar-wrapper">
         <div className="nav-link-wrapper">
-          <NavLink to={""} className="nav-bar-link">
+          <a href="*" className="nav-bar-link">
             <img
               className="nav-bar-logo"
               src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/006_f3.png"
               alt="charizard icon"
             />
-          </NavLink>
+          </a>
         </div>
         <div className="nav-bar-text">
           <h1 className="nav-title">Pokemon Super</h1>
           <span className="nav-info">
-            <p className="nav-inf-text">Home</p>
+            <a href="*">
+              <p className="nav-inf-text">Home</p>
+            </a>
             <p className="nav-inf-text">About</p>
             <p className="nav-inf-text">Info</p>
           </span>
--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import "./mainmenu.css";
 
 const MainMenu = () => {
+  const [isChoosePokemonOpen, setIsChoosePokemonOpen] =
+    useState<boolean>(false);
+
+  if (isChoosePokemonOpen) {
+    return <>123</>;
+  }
   return (
     <div className="mainmenu-container">
       <div className="mainmenu-container-wrapper">
-        <button className="mainmenu-button">Start</button>
+        <button
+          className="mainmenu-button"
+          onClick={() => setIsChoosePokemonOpen(true)}
+        >
+          Start
+        </button>
       </div>
     </div>
   );
