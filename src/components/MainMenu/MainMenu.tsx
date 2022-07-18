b31c60a3823baaadd5ea8180ceb68badb0f1cc67
Jelon <jelon1211@gmail.com>
2022-07-18 22:24:25 +0200

    pokemon index

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -5,34 +5,44 @@ const MainMenu = () => {
   const [isChoosePokemonOpen, setIsChoosePokemonOpen] =
     useState<boolean>(false);
 
+  const [isActivePokemon, setIsActivePokemon] = useState<boolean>(false);
+
   if (isChoosePokemonOpen) {
     return (
       <>
         {" "}
         <div className="mainmenu-container">
           <div className="mainmenu-container-wrapper">
-            <div>
-              <img
-                className="mainmenu-pokemon-image"
-                src="https://upload.wikimedia.org/wikipedia/en/thumb/7/73/Pikachu_artwork_for_Pok%C3%A9mon_Red_and_Blue.webp/220px-Pikachu_artwork_for_Pok%C3%A9mon_Red_and_Blue.webp.png"
-                alt="chubby pikachu"
-              />
-              <p>Pikatchu</p>
+            <div className="mainmenu-container-pokemons">
+              <div className="mainmenu-pokemon-container">
+                <img
+                  className="mainmenu-pokemon-image"
+                  src="https://upload.wikimedia.org/wikipedia/en/thumb/7/73/Pikachu_artwork_for_Pok%C3%A9mon_Red_and_Blue.webp/220px-Pikachu_artwork_for_Pok%C3%A9mon_Red_and_Blue.webp.png"
+                  alt="chubby pikachu"
+                />
+                <p className="mainmenu-pokemon-name">Pikatchu</p>
+              </div>
+              <div className="mainmenu-pokemon-container">
+                <img
+                  className="mainmenu-pokemon-image"
+                  src="https://www.pngkey.com/png/full/99-992091_adorable-bulbasaur-and-chibi-image-bulbasaur-chibi.png"
+                  alt="cute Bulbasaur"
+                />
+                <p className="mainmenu-pokemon-name">Bulbasaur</p>
+              </div>
+              <div className="mainmenu-pokemon-container">
+                <img
+                  className="mainmenu-pokemon-image"
+                  src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/006_f3.png"
+                  alt="proud charizard"
+                />
+                <p className="mainmenu-pokemon-name">Charizard</p>
+              </div>
             </div>
             <div>
-              <img
-                className="https://i.pinimg.com/originals/82/42/25/824225f61b9e0efb7e7405f8c9579f6c.png"
-                alt="angry Bulbasaur"
-              />
-              <p>Bulbasaur</p>
-            </div>
-            <div>
-              <img
-                className="mainmenu-pokemon-image"
-                src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/006_f3.png"
-                alt="proud charizard"
-              />
-              <p>Charizard</p>
+              <button className="mainmenu-findgame-button-container">
+                Find Game
+              </button>
             </div>
           </div>
         </div>
@@ -41,9 +51,9 @@ const MainMenu = () => {
   }
   return (
     <div className="mainmenu-container">
-      <div className="mainmenu-container-wrapper">
+      <div className="mainmenu-container-wrapper-home">
         <button
-          className="mainmenu-button"
+          className="mainmenu-start-button"
           onClick={() => setIsChoosePokemonOpen(true)}
         >
           Start
